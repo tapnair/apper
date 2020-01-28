56d23d9be716701eca951283747444ad88351478
Patrick Rainsberry <patrick.rainsberry@autodesk.com>
2020-01-28 14:44:28 -0800

    Added Web Requests

--- apper/__init__.py
+++ apper/__init__.py
@@ -3,6 +3,7 @@ from .FusionApp import FusionApp
 from .Fusion360AppEvents import Fusion360CustomThread
 from .Fusion360AppEvents import Fusion360DocumentEvent
 from .Fusion360AppEvents import Fusion360WorkspaceEvent
+from .Fusion360AppEvents import Fusion360WebRequestEvent
 from .Fusion360CommandBase import Fusion360CommandBase
 from .PaletteCommandBase import PaletteCommandBase
 from .Fusion360Utilities import AppObjects
