a52fcf488a5e18154f23c8608c1de1b89904d34f
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2021-05-27 14:09:49 -0700

    apper version

--- apper/__version__.py
+++ apper/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'apper'
 __description__ = 'Fusion 360 API for humans'
 __url__ = 'https://apper.readthedocs.io'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'Patrick Rainsberry'
 __author_email__ = 'patrick.rainsberry@autodesk.com'
 __license__ = 'Apache 2.0'
